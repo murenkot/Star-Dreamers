358fb5ba0f3d39da9fcde3627090f4177eb01afe
tanyakelly <murenkot@gmail.com>
2019-11-06 10:47:30 -0800

    Models are setup

--- stars_app/models.py
+++ stars_app/models.py
@@ -10,4 +10,23 @@ class Photo(models.Model):
     url = models.TextField()
     hdurl = models.TextField()
 
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts_user")
+    photo = models.ForeignKey(Photo, on_delete=models.CASCADE, related_name="posts_photo")
 
+class Comment(models.Model):
+    title = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments_user")
+    body = models.TextField()
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments_post")
+
+class Like(models.Model):
+    photo = models.ForeignKey(Photo, on_delete=models.CASCADE, related_name="likes_photo")
+    user = models.ForeignKey(Photo, on_delete=models.CASCADE, related_name="likes_user")
+
+class Profile(models.Model):
+    avatar = models.TextField()
+    userstory = models.TextField()
+    user = models.ForeignKey(Photo, on_delete=models.CASCADE, related_name="profiles_user")
