237813f184f28758b4165bd7232a1477bb0d82da
tanyakelly <murenkot@gmail.com>
2019-11-06 14:54:20 -0800

    comments for photo added

--- stars_app/views.py
+++ stars_app/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth.decorators import login_required
 
 # import Models
 from .models import Photo
+from .forms import CommentForm
 
 
 # for API requests:
@@ -49,3 +50,18 @@ def photo_details(request, pk):
     photo = Photo.objects.get(id=pk)
     context = {"photo":photo}
     return render(request, 'photo_page.html', context)
+
+def add_comment(request, pk):
+    photo = Photo.objects.get(id=pk)
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            # comment.user = request.user
+            comment.photo = photo
+            comment.save()
+            return redirect('photo_details', pk=comment.photo.pk)
+    else:
+        form = CommentForm()
+    context = {'form': form, 'photo':photo, 'header':f"Add your comment to {photo.title}"}
+    return render(request, 'photo_comment_form.html', context)
